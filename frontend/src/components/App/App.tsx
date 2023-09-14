36fe61416ef771af202d37662347aed34f0965a8
cloviswrodrigues@gmail.com <cloviswrodrigues@gmail.com>
2023-09-13 23:59:14 -0300

    partial implemented

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,21 @@
+import GlobalStyle from '../../assets/styles/global'
+import { Main, Container, Content, Navigation } from './styles'
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <Main>
+        <Container>ss
+          <Navigation>
+
+          </Navigation>
+          <Content></Content>
+        </Container>
+      </Main>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/App/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #f0f6ff;;
+`
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 120rem;
+  max-height: 50rem;
+  margin: 0 auto;
+  background-color: #ffffff;
+  border-radius: 1.5rem;
+  padding: 1.5rem;
+`
+
+export const Navigation = styled.nav`
+  background: url('../../../assets/images/bg-sidebar-desktop.svg');
+`
+
+export const Content = styled.div`
+  background-color: yellow;
+`
